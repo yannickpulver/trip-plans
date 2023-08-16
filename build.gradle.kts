9e7a270c2c9d928b00eb8a3f206ed8fa88a89857
Yannick <yannick@appswithlove.com>
2023-08-16 11:10:00 +0200

    Cleanup build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     // trick: for the same plugin versions in all sub-modules
-    kotlin("multiplatform").apply(false)
-    id("com.android.application").apply(false)
-    id("com.android.library").apply(false)
-    id("org.jetbrains.compose").apply(false)
-    id("com.google.gms.google-services").version("4.3.14").apply(false)
+    kotlin("multiplatform") apply false
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.compose") apply false
+    id("com.google.gms.google-services") version "4.3.14" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.5.1"
 }
 
